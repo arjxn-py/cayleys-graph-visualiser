7d600ec4200c3b86241a2b41f6f67595864ebb48
arjxn.py <arjxn.py@gmail.com>
2023-12-07 19:57:04 +0530

    Make function to generate cayleys graphs

--- my-vue-app/main.js
+++ my-vue-app/main.js
@@ -1,79 +1,70 @@
 import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-// Create a scene
-const scene = new THREE.Scene();
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-// Create a camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+// Function to generate Cayley's graph for a cyclic group
+const generateCayleysGraph = (groupOrder, generator) => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
 
-// Create a renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-controls.dampingFactor = 0.25;
-controls.screenSpacePanning = false;
-controls.maxPolarAngle = Math.PI / 2;
-
-// Create a group to hold the vertices
-const elements = [0, 1, 2, 3];
-const generator = 1;
-
-const verticesGroup = new THREE.Group();
-scene.add(verticesGroup);
-
-// Create vertices for each group element
-const vertices = elements.map(element => {
-  const vertex = new THREE.Mesh(new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
-  vertex.position.set(Math.cos((2 * Math.PI * element) / elements.length), Math.sin((2 * Math.PI * element) / elements.length), 0);
-  verticesGroup.add(vertex);
-  return vertex;
-});
-
-// Create edges based on group multiplication with the generator
-const edgesGroup = new THREE.Group();
-scene.add(edgesGroup);
-
-elements.forEach(element => {
-  const targetElement = (element + generator) % elements.length;
-  const lineGeometry = new THREE.BufferGeometry().setFromPoints([vertices[element].position, vertices[targetElement].position]);
-  const line = new THREE.Line(lineGeometry, new THREE.LineBasicMaterial({ color: 0xffffff }));
-  edgesGroup.add(line);
-});
-
-// Add lights
-const ambientLight = new THREE.AmbientLight(0x404040);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff);
-directionalLight.position.set(1, 1, 1).normalize();
-scene.add(directionalLight);
-
-// Render function
-const render = () => {
-  renderer.render(scene, camera);
-};
-
-// Add event listener for window resize
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-});
+  document.body.appendChild(renderer.domElement);
 
-// Initial render
-const animate = () => {
-  requestAnimationFrame(animate);
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.25;
+  controls.screenSpacePanning = false;
+  controls.maxPolarAngle = Math.PI / 2;
 
-  // Required if controls.enableDamping or controls.autoRotate are set to true
-  controls.update();
+  const verticesGroup = new THREE.Group();
+  scene.add(verticesGroup);
 
-  render();
+  const elements = Array.from({ length: groupOrder }, (_, i) => i);
+
+  const vertices = elements.map(element => {
+    const vertex = new THREE.Mesh(new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+    vertex.position.set(Math.cos((2 * Math.PI * element) / groupOrder), Math.sin((2 * Math.PI * element) / groupOrder), 0);
+    verticesGroup.add(vertex);
+    return vertex;
+  });
+
+  const edgesGroup = new THREE.Group();
+  scene.add(edgesGroup);
+
+  elements.forEach(element => {
+    const targetElement = (element + generator) % groupOrder;
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints([vertices[element].position, vertices[targetElement].position]);
+    const line = new THREE.Line(lineGeometry, new THREE.LineBasicMaterial({ color: 0xffffff }));
+    edgesGroup.add(line);
+  });
+
+  const ambientLight = new THREE.AmbientLight(0x404040);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 1, 1).normalize();
+  scene.add(directionalLight);
+
+  const render = () => {
+    renderer.render(scene, camera);
+  };
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+  };
+
+  window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
+  });
+
+  animate();
 };
 
-// Start the animation loop
-animate();
+// Call the function to generate and visualize Cayley's graph
+generateCayleysGraph(8, 2); // Example with a cyclic group of order 8 and generator 2
